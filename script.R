ab3fc2cc99d4f909399ada09a41ec307b3642be1
nelson-io <nelsonshilman@gmail.com>
2020-06-11 21:19:54 -0300

    init

---
+++ script.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(rio)
+library(naniar)
+
+# importamos datos
+
+app_alumno <- import(
+  "https://gitlab.com/AxEeduc/datos/-/raw/master/csv_files/app_alumno.csv?inline=false"
+)
+
+# codificamos NA en variables
+
+
+app_alumno %>%
+  transmute_if(is.character, as.factor) %>%
+  summary()
+
+NA_converter <- function(x) {
+  data <- ifelse(x %in% c("", "nc"), NA, x)
+  return(data)
+}
+
+NA_converter_2 <- function(x) {
+  if(is.numeric(x)){
+   ifelse(x < 0, NA, x)
+  }else{
+    x
+  }
+  
+  
+}
+
+
+
+
+
+app_alumno <- map_df(app_alumno, NA_converter)
+app_alumno <- map_df(app_alumno, NA_converter_2)
+
+
+
+glimpse(app_alumno)
+
+gg_miss_upset(app_alumno,nsets = n_var_miss(app_alumno))
+
+
+#armamos lista de DF a joinear
+model_data <- list()
+
+model_data$app_alumno <- app_alumno %>% 
+  select(!contains("ponderador_"))
